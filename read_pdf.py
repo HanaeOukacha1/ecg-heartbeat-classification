f0c0ee48e666241628bd077907051051cd1dfbc1
HanaeOukacha1 <hanaeoukacha04@gmail.com>
2026-06-04 02:11:30 +0100

    Remove read_pdf.py

--- read_pdf.py
+++
@@ -1,6 +0,0 @@
-import fitz
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-doc = fitz.open(r'c:\Users\hanae\Desktop\projet ia\Cahier_des_Charges_Projet_IA.pdf')
-for page in doc:
-    print(page.get_text())
